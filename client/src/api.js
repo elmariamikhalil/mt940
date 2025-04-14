64d9821a4eda0924163d084494cf3842f10b01cf
Khalil Elmariami <elmariamikhalil@gmail.com>
2025-04-14 09:58:38 +0100

    Update API base URL to include port number

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://main.d151ard5f4vim3.amplifyapp.com/api",
+  baseURL: "https://main.d151ard5f4vim3.amplifyapp.com:5000/api",
 });
 
 export default API;
