acaeafa70fe95105e8490e08b47e94b59376f011
Khalil Elmariami <elmariamikhalil@gmail.com>
2025-04-14 10:06:58 +0100

    Update amplify.yml to use npm ci and adjust cache paths; change API base URL to a specific IP address

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://main.d151ard5f4vim3.amplifyapp.com:5000/api",
+  baseURL: "https://13.39.246.230:5000/api",
 });
 
 export default API;
